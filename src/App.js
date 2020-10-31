215c2ada726c90b265017d5374d2b4ffa458014f
Matt1-Mac <matthewmrph40@gmail.com>
2020-10-31 03:20:30 -0400

    merge branch into main

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import PageNotFound from "./pages/PageNotFound";
+import './App.css';
+import Dashboard from "./components/home/Dashboard"
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
+import { AuthProvider } from "./components/auth/Auth"
+import PrivateRoute from "./components/auth/PrivateRoute"
+
+class App extends Component {
+  render() {
+    return (
+      <AuthProvider>
+      <Router>
+      <React.Fragment>
+          <Switch>
+            <Route exact path="/" component={SignIn} />
+            <Route path="/SignIn" component={SignIn} />
+            <Route path="/SignUp" component={SignUp} />
+            <PrivateRoute exact path="/Dashboard" component={Dashboard} />
+            <Route component={PageNotFound} />
+          </Switch>
+      </React.Fragment>
+      </Router>
+      </AuthProvider>
+    );
+  }
+}
+export default App;
+
---
+++ src/components/home/listItems.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import PeopleIcon from '@material-ui/icons/People';
+import BarChartIcon from '@material-ui/icons/BarChart';
+import LayersIcon from '@material-ui/icons/Layers';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import app from "../../base"
+
+export default function secondaryListItems() {
+  return(
+  <div>
+    <ListItem button onClick={() => app.auth().signOut()}>
+      <ListItemIcon>
+        <ExitToAppIcon />
+      </ListItemIcon>
+      <ListItemText primary="Log out" />
+    </ListItem>
+  </div>
+  );
+};
